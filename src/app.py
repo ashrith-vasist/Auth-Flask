7978390e96f7ca8e9a945de0a6e6acdc656ce389
ashrith-vasist <ashrithvasist1810@gmail.com>
2023-10-23 12:06:52 +0530

    Login page with sessions is done

--- src/app.py
+++ src/app.py
@@ -1,8 +1,28 @@
-from flask import Flask, render_template,session
+from flask import Flask, render_template,session,request,redirect,url_for
 from flask_bootstrap import Bootstrap5
+
+
+
+
+class User:
+   def __init__(self, id, username, password):
+      self.id = id
+      self.username = username
+      self.password = password
+   def __repr__(self):
+      return f'<User: {self.username}>'
+
+users=[]
+users.append(User(id=1,username='Ash@gmail.com',password='ash@2003'))
+users.append(User(id=2,username='San@gmail.com',password='San@2002'))
+
+
+
 app = Flask(__name__)
+app.secret_key='secrectkeyashonlyknows'
 bootstrap = Bootstrap5(app)
-app.secrect_key= b'_5#y2L"F4Q8z\n\xec]/'
+
+
 
 @app.route('/')
 def Home():
@@ -12,11 +32,22 @@ def Home():
 def register():
    return render_template('register.html')
 
-@app.route('/login')
+@app.route('/login',methods=('GET','POST') )
 def login():
+   if request.method == 'POST':
+      session.pop('user_id',None)
+      username = request.form['username']
+      password = request.form['password']
+      user = [x for x in users if x.username == username][0]
+      if user and user.password == password and user.username == usernamei:
+         session['user_id']=user.id
+         return redirect(url_for('Profile'))
+      return redirect(url_for('login'))
    return render_template('login.html')
 
-
+@app.route('/Profile')
+def Profile():
+   return render_template('Profile.html')
 
 if __name__ == '__main__':
    app.run()
